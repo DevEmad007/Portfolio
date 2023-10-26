e149ec32ff2ce64cbb0db21011d4dfa651ccacc8
DevEmad007 <FarshidIbtihaj@gmail.com>
2023-10-27 00:15:43 +0600

    button add

---
+++ src/components/Button/Button.jsx
@@ -0,0 +1,9 @@
+import React from 'react';
+
+const Button = ({ handleClick,className,children }) => {
+    return (
+        <button onClick={handleClick} className={className ? className : ''}>{children}</button>
+    );
+};
+
+export default Button;
