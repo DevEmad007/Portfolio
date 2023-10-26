27b41789ad9cea56fb225770e15b2cef00b8383d
DevEmad007 <FarshidIbtihaj@gmail.com>
2023-10-26 17:09:35 +0600

    header add

---
+++ src/components/DP/DP.jsx
@@ -0,0 +1,11 @@
+import React from 'react';
+import DPsrc from '../../assets/DP.jpeg';
+const DP = () => {
+    return (
+        <>
+            <img className="absolute top-14 left-5 w-28 h-28 rounded-full border-2 border-white" src={DPsrc} alt="" srcSet="" />
+        </>
+    );
+};
+
+export default DP;
---
+++ src/components/Header/Header.jsx
@@ -0,0 +1,12 @@
+import React from 'react';
+import DP from '../DP/DP';
+
+const Header = () => {
+    return (
+        <header className='h-28' style={{ backgroundImage: 'linear-gradient(to right, #370537, #6d0244, #a20f44, #ce3635, #eb6612)' }}>
+            <DP />
+        </header>
+    );
+};
+
+export default Header;
