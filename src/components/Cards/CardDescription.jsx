45c141102469851401c631b21ac76382ae2a4959
DevEmad007 <FarshidIbtihaj@gmail.com>
2023-10-28 22:11:58 +0600

    bg change

--- src/components/Cards/CardDescription.jsx
+++ src/components/Cards/CardDescription.jsx
@@ -2,7 +2,7 @@ import React from 'react';
 
 const CardDescription = ({ item }) => {
     return (
-        <div className='p-4 h-26 bg-white text-black rounded-b-2xl'>
+        <div className='p-4 h-26 bg-[#fafafa] text-black rounded-b-2xl'>
             <h2 className=' text-lg font-semibold whitespace-nowrap'>{item?.name}</h2>
             <p className=' text-base'>{item.des}</p>
         </div>
