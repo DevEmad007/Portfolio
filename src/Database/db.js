5372e39d069ffccb78bdda1ff1ca9f640394e79b
DevEmad007 <FarshidIbtihaj@gmail.com>
2023-10-28 22:17:09 +0600

    database

---
+++ src/Database/db.js
@@ -0,0 +1,123 @@
+export const ReviewsDb = [
+    {
+        "id": 1,
+        "photo": "./reviewer5.webp",
+        "name": "Savita Chakrabarti",
+        "testimonial": "This is a great project! I'm really impressed with the quality of work and the attention to detail."
+    },
+    {
+        "id": 2,
+        "photo": "./reviewer4.webp",
+        "name": " Sarah Williams",
+        "testimonial": "This project is really innovative and well-designed. I'm excited to see what the developers do next."
+    },
+    {
+        "id": 3,
+        "photo": "./reviewer3.jpg",
+        "name": "Silvia Jons",
+        "testimonial": "This project is a great example of what's possible with React. I'm glad I had the opportunity to work on it."
+    },
+    {
+        "id": 4,
+        "photo": " ./reviewer1.webp",
+        "name": "Jane Doe",
+        "testimonial": "This project was a lot of fun to work on. I'm really happy with the results."
+    },
+    {
+        "id": 5,
+        "photo": "./reviewer5.webp",
+        "name": "David Brown",
+        "testimonial": "This project is a great example of my skills and experience as a React developer."
+    }
+];
+
+export const otherReviews = [
+    {
+        name: 'Tom Hardy',
+        des: "I Told You I'm Going To Do My Job",
+        path: ''
+    },
+    {
+        name: 'Andrew Garfield',
+        des: "I Love The Idea That If You Know Someone'...",
+        path: ''
+    },
+    {
+        name: 'Patrick Bateman',
+        des: "I'm Still Here",
+        path: ''
+    },
+    {
+        name: 'Rayan Reynolds',
+        des: 'Well, I want to remember us. I swear to God...',
+        path: ''
+    },
+];
+
+export const projectsdb = [
+    {
+        name: 'Adidas Clone',
+        link: 'https://adidas-clone-3c3ed.web.app/',
+        img: './adidas.jpg',
+        des: 'Adidas clone build with React JS and Redux'
+    },
+    {
+        name: 'Social Media Site',
+        link: 'https://the-f-site.web.app/',
+        img: './social-media.jpg',
+        des: 'build with React JS,firebase'
+    },
+    {
+        name: 'Super Tic-Tac-Toe',
+        link: 'https://super-tic-tac-toe-f7b62.web.app/',
+        img: './Super-tic-tac-toe.jpg',
+        des: 'build with React JS and firebase realtime database'
+    },
+    {
+        name: 'Portfolio',
+        link: 'https://farshid-ibtihaj.web.app',
+        img: './portfolio.jpg',
+        des: 'Adidas clone build with React JS and Redux'
+    },
+];
+
+export const otherProjects = [
+    {
+        name: 'Minesweeper',
+        des: 'Vanilla JS,Firebase',
+        path: ''
+    },
+    {
+        name: 'Protfolio',
+        des: 'React Tailwind css',
+        path: ''
+    },
+    {
+        name: 'HR Shop',
+        des: 'React Bootstrap',
+        path: ''
+    },
+    {
+        name: 'Network Share App',
+        des: 'React and web',
+        path: ''
+    },
+];
+
+export const skills = [ {
+    heading: 'Frontend',
+    skils: [ 'HTML',"CSS","Javascript","Tailwind CSS","Bootstrap","React","RESTFULL API","Redux" ]
+},
+{
+    heading: 'Backend',
+    skils: [ 'PHP','lavarel' ]
+},
+{
+    heading: 'Database',
+    skils: [ 'MySQL',"MongoDB","PostgreSQL" ]
+},
+{
+    heading: 'Devops',
+    skils: [ 'GIT','GITHUB',"AWS","Firebase","Google Cloud" ]
+},
+];
