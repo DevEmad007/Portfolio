5a7dceb343adbad1bf1eb3071cea5f1bd1c5320d
DevEmad007 <FarshidIbtihaj@gmail.com>
2023-11-30 18:42:19 +0600

    responsiveness bug fix

--- src/components/Reviews/Review.jsx
+++ src/components/Reviews/Review.jsx
@@ -6,9 +6,9 @@ const Review = ({ className,item }) => {
         <Link to={`/blogs/${item?.id}`}
             className={`${className} min-w-260 max-w-3xl max-h-56 xl:max-h-72 relative rounded-3xl bg-white overflow-hidden dark:bg-slate-900`}>
             <div
-                className=' text-black dark:text-white m-3 sm:px-2 sm:m-2'>
+                className=' text-black dark:text-white p-3 lg:pt-1 xl:pt-2'>
                 <h4
-                    className='text-2xl xl:text-3xl font-semibold tracking-wide mb-[8px] sm:mb-[2px]'>
+                    className='text-2xl xl:text-3xl font-semibold tracking-wide mb-[8px] lg:mb-[0px] '>
                     {item?.name}
                 </h4>
                 <p className=' truncate whitespace-break-spaces'>
