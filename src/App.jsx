1f3b5e7c024cbadc2b4d6c5e3f60d6b19f3cbda3
DevEmad007 <FarshidIbtihaj@gmail.com>
2023-12-01 22:03:25 +0600

    on route change scroll to top add

--- src/App.jsx
+++ src/App.jsx
@@ -1,24 +1,31 @@
-import React from 'react';
+import React,{ useEffect,useState } from 'react';
 import Home from './Pages/Home';
-import { BrowserRouter,Route,Routes } from 'react-router-dom';
+import { Route,Routes,useLocation } from 'react-router-dom';
 import About from './Pages/About';
 import Blogs from './Pages/Blogs';
 import SharedLayout from './SharedLayout';
 import BlogeDetails from './Pages/BlogeDetails';
 
 const App = () => {
+  const location = useLocation();
+  const [ paramValue,setParamValue ] = useState(location.pathname);
+
+  useEffect(() => {
+    if (paramValue !== location.pathname) {
+      setParamValue(location.pathname);
+      window.scrollTo({ top: '0' });
+    }
+  },[ location.pathname ]);
 
   return (
-    <BrowserRouter>
-      <Routes>
-        <Route path='/' element={<SharedLayout />} >
-          <Route index element={<Home />} />
-          <Route path='about' element={<About />} />
-          <Route path='blogs' element={<Blogs />} />
-          <Route path='blogs/:id' element={<BlogeDetails />} />
-        </Route>
-      </Routes>
-    </BrowserRouter>
+    <Routes>
+      <Route path='/' element={<SharedLayout />} >
+        <Route index element={<Home />} />
+        <Route path='about' element={<About />} />
+        <Route path='blogs' element={<Blogs />} />
+        <Route path='blogs/:id' element={<BlogeDetails />} />
+      </Route>
+    </Routes>
   );
 };
 
